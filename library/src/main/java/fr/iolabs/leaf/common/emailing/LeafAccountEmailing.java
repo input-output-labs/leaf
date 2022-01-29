baa55a36bb0ae34a4c4a9d53c001a600b38629d1
mathiascouste <couste.mathias@gmail.com>
2022-01-29 16:40:38 +0100

    v1.8-SNAPSHOT: sendgrid integration for password forgotten sending

--- library/src/main/java/fr/iolabs/leaf/common/emailing/LeafAccountEmailing.java
+++ library/src/main/java/fr/iolabs/leaf/common/emailing/LeafAccountEmailing.java
@@ -1,5 +1,8 @@
 package fr.iolabs.leaf.common.emailing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,7 @@ import fr.iolabs.leaf.authentication.model.LeafAccount;
 
 @Service
 public class LeafAccountEmailing {
+
 	private static final String MAILGUN = "mailgun";
 	private static final String SENDGRID = "sendgrid";
 
@@ -22,9 +26,12 @@ public class LeafAccountEmailing {
 	private LeafMailgunEmailService mailgunEmailService;
 	@Autowired
 	private LeafSendgridEmailService sendgridEmailService;
-	
+
 	@Value("${sendgrid.templates.account-creation}")
 	String sendgridAccountCreationTemplateId;
+
+	@Value("${sendgrid.templates.password-change-key-sending}")
+	String sendgridPasswordChangeKeySendingTemplateId;
 
 	public void sendAccountCreationConfirmation(LeafAccount to) {
 		if (MAILGUN.equals(this.emailServiceSelection)) {
@@ -43,7 +50,11 @@ public class LeafAccountEmailing {
 			this.mailgunEmailService.sendEmail(to.getEmail(), "Votre clé de re-initialisation de mot de passe.",
 					"Clé de re-initialisation : " + resetPasswordKey, html);
 		} else if (SENDGRID.equals(this.emailServiceSelection)) {
-			// todo : implement that
+			Map<String, String> templateData = new HashMap<String, String>();
+			templateData.put("passwordChangeKey", resetPasswordKey);
+
+			this.sendgridEmailService.sendEmailWithTemplate(to.getEmail(), sendgridPasswordChangeKeySendingTemplateId,
+					templateData);
 		}
 	}
 }
--- library/src/main/java/fr/iolabs/leaf/common/emailing/LeafSendgridEmailService.java
+++ library/src/main/java/fr/iolabs/leaf/common/emailing/LeafSendgridEmailService.java
@@ -1,13 +1,16 @@
 package fr.iolabs.leaf.common.emailing;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.*;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
@@ -15,18 +18,29 @@ import com.sendgrid.SendGrid;
 
 @Service
 public class LeafSendgridEmailService {
-	@Value("${sendgrid.api.key}")
-    String sendgridAPIKey;
-    
-    @Value("${sendgrid.email.from}")
-    String sendgridEmailFrom;
+	private static Logger logger = LoggerFactory.getLogger(LeafSendgridEmailService.class);
 
-    public void sendEmailWithTemplate(String to, String templateId) {
+	@Value("${sendgrid.api.key}")
+	String sendgridAPIKey;
+
+	@Value("${sendgrid.email.from}")
+	String sendgridEmailFrom;
+
+	public void sendEmailWithTemplate(String to, String templateId) {
+		this.sendEmailWithTemplate(to, templateId, new HashMap<String, String>());
+	}
+
+	public void sendEmailWithTemplate(String to, String templateId, Map<String, String> templateData) {
 		Email from = new Email(sendgridEmailFrom);
 		Email toEmail = new Email(to);
 		Content content = new Content("text/html", "<html><body>some text here</body></html>");
 		Mail mail = new Mail(from, "Leaf email subject", toEmail, content);
 		mail.setTemplateId(templateId);
+		if (templateData != null) {
+			for (Map.Entry<String, String> data : templateData.entrySet()) {
+				mail.personalization.get(0).addDynamicTemplateData(data.getKey(), data.getValue());
+			}
+		}
 
 		SendGrid sg = new SendGrid(sendgridAPIKey);
 
@@ -36,11 +50,14 @@ public class LeafSendgridEmailService {
 			request.setEndpoint("mail/send");
 			request.setBody(mail.build());
 			Response response = sg.api(request);
-			if (response.getStatusCode() > 200 && response.getStatusCode() < 300) {
-				System.out.println("Confirmation email sent to user");
+			int statusCode = response.getStatusCode();
+			if (statusCode > 200 && statusCode < 300) {
+				logger.info("Sendgrid email sending success - ID=" + templateId);
+			} else {
+				logger.warn("Sendgrid email sending failure - ID=" + templateId + " - STATUS_CODE=" + statusCode);
 			}
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-    }
+	}
 }
